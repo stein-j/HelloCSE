5ba9c27e6c0d0c106d70fb0d9aefcde56dfbefc4
j.stein <joristein@gmail.com>
2023-03-01 14:23:33 -0500

    tailwind init

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,6 @@ export default defineConfig({
         vue(),
         laravel({
             input: [
-                'resources/css/app.css',
                 'resources/js/app.js',
             ],
             refresh: true,
